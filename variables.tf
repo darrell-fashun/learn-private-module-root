0df8b684a1d9e3813c5892a177c61a334f99b7f3
darrell-fashun <70173551+darrell-fashun@users.noreply.github.com>
2020-10-13 12:58:56 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
 variable "region" {
-  value = us-east-1
   description = "This is the cloud hosting region where your webapp will be deployed."
 }
 
 variable "prefix"{
-  value = dev-test
   description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
 }
 
 variable "name" {
-  value = darrell
   description = "Your name to attach to the webapp address"
 }
