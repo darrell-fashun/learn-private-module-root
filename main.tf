7c19513ff1a6d228e85f31e3196ecd8cdd9bdec7
darrell-fashun <70173551+darrell-fashun@users.noreply.github.com>
2020-10-13 14:57:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-support-eng/terraform-s3-webapp/aws"
+  source  = "app.terraform.io/hashicorp-support-eng/terraform-aws-s3-webapp"
   name   = var.name
   region = var.region
   prefix = var.prefix
